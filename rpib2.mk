34e2066340563e977db545fa556ea4be53aa4096
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-11-20 02:26:29 -0500

    removed device configuration for android

--- rpib2.mk
+++ rpib2.mk
@@ -1,36 +1,24 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-
-include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
-
-PRODUCT_PACKAGES += \
-    libGLES_mesa \
-    gralloc.$(TARGET_PRODUCT) \
-    hwcomposer.$(TARGET_RRODUCT)
-
-PRODUCT_COPY_FILES := \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
-    device/generic/goldfish/camera/media_codecs.xml:system/etc/media_codecs.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/rpib2_core_hardware.xml:system/etc/permissions/rpib2_core_hardware.xml \
-    $(LOCAL_PATH)/init.rpib2.rc:root/init.rpib2.rc \
-    $(LOCAL_PATH)/init.usb.rc:root/init.usb.rc \
-    $(LOCAL_PATH)/fstab.rpib2:root/fstab.rpib2 \
-    $(LOCAL_PATH)/Generic.kl:system/usr/keylayout/Generic.kl \
-    $(PRODUCT_COPY_FILES)
-
-DEVICE_PACKAGE_OVERLAYS := device/brcm/rpib2/overlay
-PRODUCT_CHARACTERISTICS := tablet,nosdcard
-PRODUCT_LOCALES := en_US
-
-PRODUCT_AAPT_CONFIG :=nomal tvdpi hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+$(call inherit-product, device/raspberrypi/rpib2/device.mk)
 
 PRODUCT_NAME := rpib2
 PRODUCT_DEVICE := rpib2
-PRODUCT_BRAND := raspberry
-PRODUCT_MANUFACTER := brcm
+PRODUCT_BRAND := Firefox OS
+PRODUCT_MANUFACTER := raspberrypi
 PRODUCT_MODEL := rpib2
