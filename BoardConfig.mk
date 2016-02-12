d97b0d4af37a45c2aa42dfe3f6300cc6983c2837
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2016-02-12 15:40:01 +0100

    Bug 1242402 - Produce boot.img for RPi2 r=_AtilA_

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,10 +36,30 @@ TARGET_PRELINK_MODULE := false
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 TARGET_USERIMAGES_USE_EXT4 := true
 
+# It looks like we cannot use 32MB with FAT32. But 48MB works.
+BOARD_BOOTIMAGE_PARTITION_SIZE := 50331648 # 48M
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912 # 512M
 BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728 # 128M
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 134217728 # 128M
+
+PRODUCT_SUPPORTS_VERITY := false
+TARGET_BOOTIMAGE_USE_EXT2 := false
+TARGET_BOOTIMAGE_USE_EXTLINUX := false
+TARGET_BOOTIMAGE_USE_VFAT := true
+
+BOARD_CUSTOM_MKBOOTIMG := device/raspberrypi/rpi2b/mkbootimg_rpi2
+BOARD_MKBOOTIMG_ARGS := \
+    --bootcode   device/raspberrypi/rpi2b/boot/bootcode.bin \
+    --configtxt  device/raspberrypi/rpi2b/boot/config.txt \
+    --dt         $(ANDROID_PRODUCT_OUT)/dtbs/bcm2709-rpi-2-b.dtb \
+    --fixupdat   device/raspberrypi/rpi2b/boot/fixup.dat \
+    --startelf   device/raspberrypi/rpi2b/boot/start.elf \
+    --partsize   $(BOARD_BOOTIMAGE_PARTITION_SIZE)
+
+BOARD_KERNEL_CMDLINE := initrd=0x01f00000 dwc_otg.lpm_enable=0
+BOARD_KERNEL_CMDLINE += console=ttyAMA0,115200 no_console_suspend root=/dev/ram0
+BOARD_KERNEL_CMDLINE += elevator=deadline rootwait androidboot.hardware=rpi2b
 
 BOARD_FLASH_BLOCK_SIZE := 4096
 BOARD_MALLOC_ALIGNMENT := 16
@@ -53,4 +73,3 @@ USE_OPENGL_RENDERER := true
 TARGET_USE_PAN_DISPLAY := true
 
 USE_CAMERA_STUB := true
-
